247b9fbc33d12cdf23b335e81589e4e2dc8fc54c
Muhammad Farid <mfarid2121@gmail.com>
2024-10-06 07:29:10 +0700

    add order utils

---
+++ FoodOnline.Core/Utils/OrderUtils.cs
@@ -0,0 +1,17 @@
+﻿namespace FoodOnline.Core.Utils;
+
+public class OrderUtils
+{
+    private const string FORMAT = "yyyyMMdd";
+
+    public static string GenerateCode()
+    {
+        var now = DateTime.UtcNow;
+        return now.ToString(FORMAT);
+    }
+
+    public static string GenerateCode(DateTime datetime)
+    {
+        return datetime.ToString(FORMAT);
+    }
+}
