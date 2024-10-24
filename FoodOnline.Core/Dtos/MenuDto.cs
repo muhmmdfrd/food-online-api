91a3bd723954bc3140ebeb25175ab8d8f3269a01
Muhammad Farid <mfarid2121@gmail.com>
2024-10-24 15:22:17 +0700

    add properties for menuViewDto

--- FoodOnline.Core/Dtos/MenuDto.cs
+++ FoodOnline.Core/Dtos/MenuDto.cs
@@ -4,36 +4,36 @@ namespace FoodOnline.Core.Dtos;
 
 public class MenuDto
 {
-    public long Id { get; set; }
-
     public string Name { get; set; } = null!;
-
     public int DataStatusId { get; set; }
-
     public int Price { get; set; }
-
     public long MerchantId { get; set; }
     public string? Description { get; set; }
     public string? Code { get; set; }
-    public long? CreatedBy { get; set; }
-
-    public DateTime? CreatedAt { get; set; }
-
-    public long? ModifiedBy { get; set; }
-
-    public DateTime? ModifiedAt { get; set; }
 }
 
 public class MenuViewDto : MenuDto
 {
+    public long Id { get; set; }
     public string DataStatusName { get; set; } = null!;
     public string MerchantName { get; set; } = null!;
-    public string? Picture { get; set; }
+    public string? Picture => string.IsNullOrEmpty(Code) ? $"http:127.0.0.1:8000/api/files/{Code}" : null;
 }
 
-public class MenuAddDto : MenuDto{}
+public class MenuAddDto : MenuDto
+{
+    public long? CreatedBy { get; set; }
+    public DateTime? CreatedAt { get; set; }
+    public long? ModifiedBy { get; set; }
+    public DateTime? ModifiedAt { get; set; }
+}
 
-public class MenuUpdDto : MenuDto{}
+public class MenuUpdDto : MenuDto
+{
+    public long Id { get; set; }
+    public long? ModifiedBy { get; set; }
+    public DateTime? ModifiedAt { get; set; }
+}
 
 public class MenuFilter : TableFilter
 {
